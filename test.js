975238231184324471873f121a768ea9498e22ae
Ivan Lazarev <lazarevivan@googlemail.com>
2012-10-03 15:28:38 -0400

    ololo

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 var http = require('http');
 http.createServer(function (req, res) {
    res.writeHead(200, {'Content-Type': 'text/plain'});
-   res.end('Hello World\n');
-}).listen(process.env.PORT); 
+   res.end('Tmm rules !\n');
+}).listen(process.env.PORT);
